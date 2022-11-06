c9f2cef6f292c93e3b795c2146882c04e7990758
Tiago Oliveira <dev.tiago.oliveira.com>
2022-11-05 22:06:31 -0300

    feat: ensure SignUpController returns 400 if no name is provided

--- src/presentation/controllers/SignUpController.php
+++ src/presentation/controllers/SignUpController.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 namespace Otaodev\PhpBestPractice\presentation\controllers;
 
+use Error;
+
 class SignUpController
 {
     public function handle(): string|false
     {
+        $error = new Error('Missing param: name');
         return json_encode([
-            'statusCode' => 400
+            'statusCode' => 400,
+            'body' => $error
         ]);
     }
 }
